711d7d287c8e6a3635c659ca0d3bed49fb3bc98e
FROM-minyoung <918liter@gmail.com>
2023-04-04 00:01:21 +0900

    init: Router component 설정

--- front-end/src/router.tsx
+++ front-end/src/router.tsx
@@ -1,1 +1,15 @@
-// test
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
