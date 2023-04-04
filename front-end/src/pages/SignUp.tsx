20389228964640812c5fcf39806fc4a6911121ee
FROM-minyoung <918liter@gmail.com>
2023-04-04 16:16:01 +0900

    feat: 회원가입 api 연결(#1)

--- front-end/src/pages/SignUp.tsx
+++ front-end/src/pages/SignUp.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import useMakeEmail from "../hooks/useMakeEmail";
 import useMakePassWord from "../hooks/useMakePassWord";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const { userEmail, isConfirmEmail, handleEmailInput } = useMakeEmail();
   const { userPassWord, isConfirmPassWord, handlePassWordInput } =
     useMakePassWord();
+
+  const navigate = useNavigate();
+
+  async function handleSignUp() {
+    await fetch("https://www.pre-onboarding-selection-task.shop/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: userEmail, password: userPassWord }),
+    })
+      .then((data) => {
+        if (data.status === 201) {
+          alert("회원가입이 완료되었습니다.");
+          navigate("/signin");
+        } else throw data.status;
+      })
+      .catch((error) =>
+        alert(`회원가입 중 에러가 발생했습니다. \n에러코드 : ${error}`)
+      );
+  }
 
   return (
     <div>
@@ -27,6 +49,7 @@ export default function SignUp() {
           data-testid="signup-button"
           type="button"
           disabled={!(isConfirmEmail && isConfirmPassWord)}
+          onClick={handleSignUp}
         >
           회원가입
         </button>
