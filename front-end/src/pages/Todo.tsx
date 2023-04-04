41e762e1544a18fca3b9382f2eb4001f6611d1cb
FROM-minyoung <918liter@gmail.com>
2023-04-04 19:43:36 +0900

    feat: user가 입력한 Todo input state 제작(#3)

--- front-end/src/pages/Todo.tsx
+++ front-end/src/pages/Todo.tsx
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
+import useMakeUserTodo from "../hooks/useMakeUserTodo";
+
 export default function Todo() {
+  const { userTodo, handleUserTodo } = useMakeUserTodo();
   return (
     <div>
       <span>TODO</span>
-      <input type="text" data-testid="new-todo-input" />
+      <input
+        type="text"
+        data-testid="new-todo-input"
+        value={userTodo}
+        onChange={handleUserTodo}
+      />
       <button type="button" data-testid="new-todo-add-button">
         추가
       </button>
