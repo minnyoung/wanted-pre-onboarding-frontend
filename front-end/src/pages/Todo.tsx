b853b7bd30d529f10a331ba5f9b669d0c8f28ddf
FROM-minyoung <918liter@gmail.com>
2023-04-04 21:29:39 +0900

    feat: get todo api 연결, todo 추가시 새로운 todo로 추가되게 함(#4)

--- front-end/src/pages/Todo.tsx
+++ front-end/src/pages/Todo.tsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useMakeUserTodo from "../hooks/useMakeUserTodo";
+
+type TodoListType = {
+  id: number;
+  todo: string;
+  isCompleted: boolean;
+  userId: string;
+};
 
 export default function Todo() {
   const { userTodo, handleUserTodo } = useMakeUserTodo();
+  const [todoList, setTodoList] = useState<TodoListType[]>([]);
   const userToken = localStorage.getItem("userToken");
+
+  useEffect(fetchReadTodoList, []);
+
+  function fetchReadTodoList() {
+    fetch("https://www.pre-onboarding-selection-task.shop/todos", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTodoList(data);
+      });
+  }
 
   async function fetchCreateTodoList() {
     await fetch("https://www.pre-onboarding-selection-task.shop/todos", {
@@ -18,6 +41,7 @@ export default function Todo() {
     })
       .then((response) => {
         if (response.status !== 201) throw new Error(`${response.status}`);
+        fetchReadTodoList();
       })
       .catch((error) =>
         alert(`TODO를 등록하던 중 에러가 발생했습니다. \n에러내용 ${error}`)
@@ -40,7 +64,14 @@ export default function Todo() {
       >
         추가
       </button>
-      <div>todoList 보이는 곳</div>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            <input type="checkbox" checked={todo.isCompleted} />
+            {todo.todo}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
