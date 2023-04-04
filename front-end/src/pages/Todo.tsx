29076e1b7edee3c90fc283868480a1f7fcb7b1d3
FROM-minyoung <918liter@gmail.com>
2023-04-04 20:18:36 +0900

    feat: create todo api 연결(#3)

--- front-end/src/pages/Todo.tsx
+++ front-end/src/pages/Todo.tsx
@@ -3,6 +3,27 @@ import useMakeUserTodo from "../hooks/useMakeUserTodo";
 
 export default function Todo() {
   const { userTodo, handleUserTodo } = useMakeUserTodo();
+  const userToken = localStorage.getItem("userToken");
+
+  async function fetchCreateTodoList() {
+    await fetch("https://www.pre-onboarding-selection-task.shop/todos", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        todo: `${userTodo}`,
+      }),
+    })
+      .then((response) => {
+        if (response.status !== 201) throw new Error(`${response.status}`);
+      })
+      .catch((error) =>
+        alert(`TODO를 등록하던 중 에러가 발생했습니다. \n에러내용 ${error}`)
+      );
+  }
+
   return (
     <div>
       <span>TODO</span>
@@ -12,7 +33,11 @@ export default function Todo() {
         value={userTodo}
         onChange={handleUserTodo}
       />
-      <button type="button" data-testid="new-todo-add-button">
+      <button
+        type="button"
+        data-testid="new-todo-add-button"
+        onClick={fetchCreateTodoList}
+      >
         추가
       </button>
       <div>todoList 보이는 곳</div>
