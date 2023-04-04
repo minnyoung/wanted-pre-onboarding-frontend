63cab7558af9aa4642d307d6cba2b7ee0428b81a
FROM-minyoung <918liter@gmail.com>
2023-04-04 15:24:47 +0900

    feat: 회원가입 이메일/비밀번호 유효성검사 추가(#1)

--- front-end/src/pages/SignUp.tsx
+++ front-end/src/pages/SignUp.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import useMakeEmail from "../hooks/useMakeEmail";
+import useMakePassWord from "../hooks/useMakePassWord";
 
 export default function SignUp() {
+  const { userEmail, isConfirmEmail, handleEmailInput } = useMakeEmail();
+  const { userPassWord, isConfirmPassWord, handlePassWordInput } =
+    useMakePassWord();
+
   return (
     <div>
       <span>회원가입</span>
       <div>
-        <input data-testid="email-input" type="email" />
-        <input data-testid="password-input" type="password" />
-        <button data-testid="signup-button" type="button">
+        <input
+          data-testid="email-input"
+          type="email"
+          value={userEmail}
+          onChange={handleEmailInput}
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          value={userPassWord}
+          onChange={handlePassWordInput}
+        />
+        <button
+          data-testid="signup-button"
+          type="button"
+          disabled={!(isConfirmEmail && isConfirmPassWord)}
+        >
           회원가입
         </button>
       </div>
