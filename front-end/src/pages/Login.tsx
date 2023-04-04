389641b4607b3d8e2509bc299befba864136aa46
FROM-minyoung <918liter@gmail.com>
2023-04-04 16:34:01 +0900

    feat: 로그인 api 연결(#2)

--- front-end/src/pages/Login.tsx
+++ front-end/src/pages/Login.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 import useMakeEmail from "../hooks/useMakeEmail";
 import useMakePassWord from "../hooks/useMakePassWord";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const { userEmail, isConfirmEmail, handleEmailInput } = useMakeEmail();
   const { userPassWord, isConfirmPassWord, handlePassWordInput } =
     useMakePassWord();
+  const navigate = useNavigate();
+
+  async function handleSignIn() {
+    await fetch("https://www.pre-onboarding-selection-task.shop/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: userEmail, password: userPassWord }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.access_token) {
+          localStorage.setItem("userToken", data.access_token);
+          alert("로그인되었습니다.");
+          navigate("/todo");
+        } else throw data.message;
+      })
+      .catch((error) =>
+        alert(`로그인 중 에러가 발생했습니다. \n에러내용: ${error}`)
+      );
+  }
 
   return (
     <div>
@@ -27,6 +50,7 @@ export default function Login() {
           data-testid="signin-button"
           type="button"
           disabled={!(isConfirmEmail && isConfirmPassWord)}
+          onClick={handleSignIn}
         >
           로그인
         </button>
