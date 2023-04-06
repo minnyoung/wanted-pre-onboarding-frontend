93acad65d4ed96bd8514c81310be69f410ff3179
FROM-minyoung <918liter@gmail.com>
2023-04-06 18:07:29 +0900

    feat: 메인페이지에서 토큰 유무에 따라 /todo 리다이렉트 구현

--- front-end/src/pages/Main.tsx
+++ front-end/src/pages/Main.tsx
@@ -1,7 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Main() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("userToken")) {
+      navigate("/todo");
+    }
+  });
+
   return (
     <div>
       <span>TODO</span>
