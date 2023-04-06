2469a35e7d7e150350c86ac40ede3cbad6d1b0f4
FROM-minyoung <918liter@gmail.com>
2023-04-06 20:58:59 +0900

    style: 회원가입 page style 적용

--- front-end/src/pages/SignUp.tsx
+++ front-end/src/pages/SignUp.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import useMakeEmail from "../hooks/useMakeEmail";
 import useMakePassWord from "../hooks/useMakePassWord";
 import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function SignUp() {
   const { userEmail, isConfirmEmail, handleEmailInput } = useMakeEmail();
@@ -36,34 +37,99 @@ export default function SignUp() {
   }
 
   return (
-    <div>
-      <span>회원가입</span>
-      <span>계정이 있으신가요?</span>
-      <Link to="/signin">로그인</Link>
+    <S.SignupContainer>
+      <S.SignupLogoSpan>회원가입</S.SignupLogoSpan>
       <div>
-        <input
-          placeholder="이메일을 입력해주세요."
-          data-testid="email-input"
-          type="email"
-          value={userEmail}
-          onChange={handleEmailInput}
-        />
-        <input
-          placeholder="8자리 이상 입력해주세요."
-          data-testid="password-input"
-          type="password"
-          value={userPassWord}
-          onChange={handlePassWordInput}
-        />
-        <button
+        <S.SignupInputContainer>
+          <span>이메일</span>
+          <input
+            placeholder="example@mail.com"
+            data-testid="email-input"
+            type="email"
+            value={userEmail}
+            onChange={handleEmailInput}
+          />
+        </S.SignupInputContainer>
+        <S.SignupInputContainer>
+          <span>비밀번호</span>
+          <input
+            placeholder="비밀번호"
+            data-testid="password-input"
+            type="password"
+            value={userPassWord}
+            onChange={handlePassWordInput}
+          />
+        </S.SignupInputContainer>
+        <S.SignupButton
           data-testid="signup-button"
           type="button"
           disabled={!(isConfirmEmail && isConfirmPassWord)}
           onClick={handleSignUp}
         >
           회원가입
-        </button>
+        </S.SignupButton>
+        <S.SignupDescContainer>
+          <span>계정이 있으신가요?</span>
+          <Link to="/signin">로그인</Link>
+        </S.SignupDescContainer>
       </div>
-    </div>
+    </S.SignupContainer>
   );
 }
+
+const S = {
+  SignupContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `,
+  SignupLogoSpan: styled.span`
+    margin-bottom: 20px;
+    font-size: 30px;
+    font-weight: 800;
+  `,
+  SignupInputContainer: styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 6px;
+    width: 220px;
+
+    span {
+      width: 55px;
+      font-size: 15px;
+      text-align: center;
+    }
+
+    input {
+      width: 140px;
+      font-size: 14px;
+      font-weight: 600;
+    }
+  `,
+
+  SignupButton: styled.button`
+    margin: 10px 0;
+    width: 100%;
+    height: 35px;
+    border: 1px solid gray;
+    border-radius: 10px;
+    :hover {
+      background-color: #f0f0f0;
+      transition: 0.1s ease-in-out;
+    }
+  `,
+  SignupDescContainer: styled.div`
+    display: flex;
+    justify-content: center;
+    font-size: 13px;
+    gap: 5px;
+    span {
+      color: gray;
+    }
+    a {
+      :hover {
+        text-decoration: underline;
+      }
+    }
+  `,
+};
