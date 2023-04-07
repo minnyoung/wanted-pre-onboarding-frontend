82cf546a4f15b04bebebd59d1e1dc3d5136f3608
FROM-minyoung <918liter@gmail.com>
2023-04-07 18:01:14 +0900

    style: 404 page style 적용

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,38 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export default function NotFound() {
   return (
-    <div>
+    <S.NotFoundContainer>
+      <S.NotFoundNumberContainer>
+        <span>404</span>
+      </S.NotFoundNumberContainer>
       <span>잘못된 주소입니다!</span>
       <Link to="/">홈으로 이동하기</Link>
-    </div>
+    </S.NotFoundContainer>
   );
 }
+
+const S = {
+  NotFoundContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    span {
+      margin-bottom: 10px;
+    }
+    a {
+      text-decoration: underline;
+    }
+  `,
+
+  NotFoundNumberContainer: styled.div`
+    span {
+      font-size: 200px;
+      font-weight: 900;
+      opacity: 0.3;
+      color: #bebebe;
+    }
+  `,
+};
